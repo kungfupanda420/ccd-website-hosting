10b7170d548318fee1fa8f7702c1470822b4bc2d
Pranav Prakash <pranavpprakash7@gmail.com>
2023-07-24 09:00:55 +0530

    Update formSubmit.js

--- server/routes/formSubmit.js
+++ server/routes/formSubmit.js
@@ -7,7 +7,7 @@ router.post('/', function(req, res) {
   
   //const message = `Dear ${name},\n\nThank you for expressing interest in recruiting from our college. We have received your submission and are thrilled to have the opportunity to collaborate with you. Our team will review your details and reach out to you soon to discuss further steps. We appreciate your interest and look forward to the possibility of building a fruitful partnership.\n\nBest regards,\nCentre For Career Development\nNIT Calicut`;
   
-  const errmessage = `Dear ${name},\n\n Thank you for considering our college for recruiting opportunities. We apologize for any inconvenience caused by the ongoing website maintenance. In the meantime, please feel free to reach out to us via email at \nccd@nitc.ac.in\n for any inquiries or further information regarding the recruitment process `;
+  const errmessage = `Dear ${name},\n\nThank you for considering our college for recruiting opportunities. We apologize for any inconvenience caused by the ongoing website maintenance. In the meantime, please feel free to reach out to us via email at \n\nccd@nitc.ac.in\n\nfor any inquiries or further information regarding the recruitment process `;
   res.send(errmessage);
 });
 
