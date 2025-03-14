575f9e6deae24337a47408a6adeaa4163459dbe7
EMANDI DEVI PRANEETH <praneethe4@gmail.com>
2025-03-14 14:58:46 +0530

    size changes

--- backend/index.js
+++ backend/index.js
@@ -153,8 +153,8 @@ const fs = require("fs");
 
 const app = express();
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ extended: true,limit: '100mb' }));
 
 // Database Connection
 const db = mysql.createConnection({
@@ -195,7 +195,11 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage });
+const upload = multer({
+  limits: { fileSize: 100 * 1024 * 1024 }, // Set max file size to 100MB
+  storage,
+});
+
 
 // Fetch filtered mentors
 app.post("/filter", (req, res) => {
