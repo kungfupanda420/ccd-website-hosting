35ca76e579b1a3f2ad2ee00d22287a6b74df0bd0
thomasjames433 <thomaspjames433@gmail.com>
2025-06-24 17:17:02 +0530

    .

--- fastapi-backend/routers/professors.py
+++ fastapi-backend/routers/professors.py
@@ -59,22 +59,25 @@ def show_projects(db:Session=Depends(get_db),current_user: User=Depends(get_curr
     projects=db.query(Project).filter(Project.professor_id==current_user.id).all()
     return projects
 
+#ADMIN
 @router.put("/projects/{id}",response_model=ShowProject) #Working
 def edit_project(id:int,request:ProjectCreation,db:Session=Depends(get_db),current_user: User=Depends(get_current_user)):
     
-    
-    if(current_user.role != 'professor'):
+    if current_user.role != 'professor' and current_user.role != 'admin':
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to edit project")
     
     round=db.query(Round).filter(Round.id==1).first()
     if round.number!=0:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Cannot modify a project once allotment starts")
-    
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Cannot modify a project once allotment starts")
+        
     project=db.query(Project).filter(Project.id==id).first()
     if not project:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Project not found")
-    if not project.professor_id == current_user.id:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to edit project")
+    
+    if(current_user.role == 'professor'):
+        if not project.professor_id == current_user.id:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to edit project")
+    
     
     project.title=request.title
     project.description=request.description
@@ -89,23 +92,27 @@ def edit_project(id:int,request:ProjectCreation,db:Session=Depends(get_db),curre
     db.refresh(project)
     return project
 
+#ADMIN
 @router.delete('/project/{id}',status_code=status.HTTP_204_NO_CONTENT) #Working
 def delete_project(id:int,db:Session=Depends(get_db),current_user: User=Depends(get_current_user)):
-    if(current_user.role != 'professor'):
+    if current_user.role != 'professor' and current_user.role != 'admin':
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to delete project")
     
     round=db.query(Round).filter(Round.id==1).first()
     if round.number!=0:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Cannot delete a project once allotment starts")
     
+
     project=db.query(Project).filter(Project.id==id).first()
     if not project:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Project not found")
-    if not project.professor_id == current_user.id:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to delete project")
+    if(current_user.role == 'professor'):
+        if not project.professor_id == current_user.id:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to delete project")
     db.delete(project)
     db.commit()
     return
+
 
 @router.get("/allotted_student")
 def get_alloted_students(db:Session=Depends(get_db),current_user: User=Depends(get_current_user)):
@@ -128,10 +135,11 @@ def get_alloted_students(db:Session=Depends(get_db),current_user: User=Depends(g
         for student in students
     ]
 
+#ADMIN
 @router.post('/set_start_date/{sip_id}')
 def set_start_date(sip_id:str, request:SetDate, db:Session=Depends(get_db),current_user: User=Depends(get_current_user)):
-    if(current_user.role != 'professor'):
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to delete project")
+    if current_user.role != 'professor' and current_user.role != 'admin':
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to set start date")
     
     
     student = (
@@ -146,21 +154,22 @@ def set_start_date(sip_id:str, request:SetDate, db:Session=Depends(get_db),curre
     if not student:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Student not found")
     
-    if student.selected_project.professor_id != current_user.id:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="You are not authorized to update this student's internship dates."
-        )
+    if(current_user.role == 'professor' ):
+        if student.selected_project.professor_id != current_user.id:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="You are not authorized to update this student's internship dates."
+            )
     
     student.start_date=request.date
     db.commit()
     db.refresh(student)
     return {"message": f"Student has started internship on {student.start_date}"}
 
-
+#ADMIN
 @router.post('/set_end_date/{sip_id}')
 def set_end_date(sip_id:str, request:SetDate, db:Session=Depends(get_db),current_user: User=Depends(get_current_user)):
-    if(current_user.role != 'professor'):
+    if current_user.role != 'professor' and current_user.role != 'admin':
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to delete project")
     
     student = (
@@ -176,21 +185,23 @@ def set_end_date(sip_id:str, request:SetDate, db:Session=Depends(get_db),current
     if not student:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Project not found")
     
-    if student.selected_project.professor_id != current_user.id:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="You are not authorized to update this student's internship dates."
-        )
+    if(current_user.role == 'professor' ):
+        if student.selected_project.professor_id != current_user.id:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="You are not authorized to update this student's internship dates."
+            )
+    
 
     student.end_date=request.date
     db.commit()
     db.refresh(student)
     return {"message": f"Student has ended internship on {student.end_date}"}
 
-
+#ADMIN
 @router.post('/project_report/{sip_id}')
 def approve_report(sip_id:str, db:Session=Depends(get_db),current_user: User=Depends(get_current_user)):
-    if(current_user.role != 'professor'):
+    if current_user.role != 'professor' and current_user.role != 'admin':
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to delete project")
     
     student = (
@@ -207,11 +218,13 @@ def approve_report(sip_id:str, db:Session=Depends(get_db),current_user: User=Dep
     if not student:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Project not found")
     
-    if student.selected_project.professor_id != current_user.id:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="You are not authorized to approve this student's report."
-        )
+    if(current_user.role == 'professor' ):
+        if student.selected_project.professor_id != current_user.id:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="You are not authorized to update this student's internship details."
+            )
+    
     if not student.project_report_path:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
@@ -225,10 +238,10 @@ def approve_report(sip_id:str, db:Session=Depends(get_db),current_user: User=Dep
 import os
 from fastapi.responses import StreamingResponse, FileResponse
 
-
+#ADMIN
 @router.get("/project_report/{sip_id}",)
 def student_project_report(sip_id:str, db: Session=Depends(get_db),current_user: User=Depends(get_current_user)):
-    if current_user.role != 'professor':
+    if current_user.role != 'professor' and current_user.role != 'admin':
         raise HTTPException(status_code=403, detail="You are not authorized to access this resource")
     student = (
         db.query(Student)
@@ -247,6 +260,13 @@ def student_project_report(sip_id:str, db: Session=Depends(get_db),current_user:
 
     if not os.path.isfile(file_path):
         raise HTTPException(status_code=404, detail="Project Report card not found")
+    
+    if(current_user.role == 'professor' ):
+        if student.selected_project.professor_id != current_user.id:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="You are not authorized to view this student's internship details."
+            )
     
     return FileResponse(
         path=file_path,
---
+++ fastapi-backend/schemas/professor.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel, Field
+from typing import Optional, List
+
+from .projects import DepartmentName, ShowProject
+from .users import ShowUser
+
+class ShowProfessorDet(BaseModel):
+    
+    name:str
+    user_det:ShowUser
+    department:DepartmentName
+    model_config = {
+        "from_attributes": True
+    }
+
+
+class ShowProfessorProj(BaseModel):
+    name:str
+    user_det:ShowUser
+    department:DepartmentName
+    projects: Optional[List[ShowProject]] 
+
+    model_config = {
+        "from_attributes": True
+    }
